6c79b81bc569126908b9977c3a21310c2651e77d
LE GARS Florian <legars.florian@gmail.com>
2019-03-06 16:35:57 +0100

    ars-20 : Refactoring admin

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Tag
      */
     private $label;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Anime", mappedBy="tag")
+     */
+    private $animes;
+
+    public function __construct()
+    {
+        $this->animes = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -37,5 +49,38 @@ class Tag
         $this->label = $label;
 
         return $this;
+    }
+
+    /**
+     * @return Collection|Anime[]
+     */
+    public function getAnimes(): Collection
+    {
+        return $this->animes;
+    }
+
+    public function addAnime(Anime $anime): self
+    {
+        if (!$this->animes->contains($anime)) {
+            $this->animes[] = $anime;
+            $anime->addTag($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnime(Anime $anime): self
+    {
+        if ($this->animes->contains($anime)) {
+            $this->animes->removeElement($anime);
+            $anime->removeTag($this);
+        }
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->label;
     }
 }
