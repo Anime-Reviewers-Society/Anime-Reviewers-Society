9dc2fb1730f99ba1118ed97c9db09485223efaa2
LE GARS Florian <legars.florian@gmail.com>
2019-03-07 15:19:25 +0100

    User profil + user edit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,23 +2,47 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class UserController extends AbstractController
 {
+    /**
+     * @return Response
+     * @Route("/profil", name="user.show")
+     */
+    public function profil(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('users/user.html.twig', [ 'user' => $user]);
+    }
 
     /**
-     * @param UserRepository $userRepository
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return RedirectResponse
      *
-     * * @Route("/user/{id}", name="user.show")
+     * @Route("/profil/edit", name="profil.edit")
      */
-    public function show(UserRepository $userRepository, int $id): Response
+    public function edit(Request $request): Response
     {
-        $user = $userRepository->find($id);
-        return $this->render('users/user.html.twig', [ 'user' => $user]);
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user.show');
+        }
+
+        return $this->render('users/user.edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
