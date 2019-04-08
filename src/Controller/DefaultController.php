f75562a28ea756417e47300cbc33d28f30d2c71f
Diane Mansuy <t-dmansuy@egencia.com>
2019-04-08 17:16:14 +0200

    create contact form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Form\php;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -36,6 +40,7 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
     /**
      * @Route("/ars", name="ars")
      */
@@ -43,6 +48,18 @@ class DefaultController extends AbstractController
     {
         return $this->render('pages/pageArs.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        $form = $this->createForm(ContactType::class);
+
+        return $this->render('pages/pageContact.html.twig', [
+            'contact_form' => $form->createView(),
         ]);
     }
 }
